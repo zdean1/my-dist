525757d3166d0fa72357b2324a055a695a8dd05d
Zach Dean <zdean3721@gmail.com>
2019-09-30 15:05:02 -0500

    service method to change boolean value added

--- src/app/my-service.service.ts
+++ src/app/my-service.service.ts
@@ -8,8 +8,6 @@ export class MyServiceService {
   constructor() { }
 
   likeImage(boolParam: boolean) {
-    console.log('boolParam = ' + boolParam);
-    console.log('bool Param set to ' + !boolParam);
     if (boolParam == false) {
       return true;
     } else { return false; }
