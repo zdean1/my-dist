3647c24244316d14835caa2d59ab2c1a3e10e849
Zach Dean <zdean3721@gmail.com>
2019-10-01 13:41:30 -0500

    messages service added

---
+++ src/app/messages.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { User } from './models/user';
+import { Message } from './models/Message';
+import { Messages } from './models/Messages';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagesService {
+
+  constructor() { }
+
+
+}
