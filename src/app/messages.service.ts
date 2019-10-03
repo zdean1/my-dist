0c31a9e83f1c55155a5f3199fc7e79c5e6770826
Zach Dean <zdean3721@gmail.com>
2019-10-02 20:34:43 -0500

    added function to instantiate messages and pushed to Messages object

--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -11,5 +11,24 @@ export class MessagesService {
 
   constructor() { }
 
+  messagesArray = new Messages();
 
+  newMessages() {
+    var user1 = new User('Zach');
+    var user2 = new User('Josh');
+
+    var message1 = new Message(user2.name, 'Just saw the new picture', new Date());
+    var message2 = new Message(user2.name, 'Think it is awesome!', new Date());
+    var message3 = new Message(user1.name, 'Thanks! Really appreciate it', new Date());
+
+
+    this.messagesArray.pushMessages(message1);
+    this.messagesArray.pushMessages(message2);
+    this.messagesArray.pushMessages(message3);
+
+  }
+
+  getMessages() {
+    return this.messagesArray.getMessages();
+  }
 }
